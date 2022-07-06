167193ac8bc8fe5cefc6d1c1d6984ebec4ef5ec2
carla-piazza1 <91873098+carla-piazza1@users.noreply.github.com>
2022-07-06 10:17:05 +0200

    mise en forme de la page d'acceuil

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="view/css/index.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <div class="div1">
+        <div class="div1-1" data-aos="fade-right" data-aos-duration="1500">
+            <h1>H3 Headline</h1>
+                <p>
+                
+                    Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet;
+                    illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, 
+                    ut ne quando amare inciperemus eum, 
+                    quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, 
+                    ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.
+                </p>
+        </div>
+        
+            <img src="view/img/bio1.JPG" alt="photo du coach" data-aos="fade-left" data-aos-duration="1500">
+
+
+    </div> 
+    <div class="div2" data-aos="fade-left" data-aos-duration="1500">
+    <img src="view/img/bio2.JPG" alt="photo du coach" data-aos="fade-right" data-aos-duration="1500">
+        <div class="div2-2">
+            <h1>H3 Headline</h1>
+            
+                <p>
+                    Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet;
+                    illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, 
+                    ut ne quando amare inciperemus eum, 
+                    quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, 
+                    ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.
+                </p>
+        </div>
+        
+
+    </div> 
+    <div class="div3">
+        <div class="div3-3" data-aos="fade-right" data-aos-duration="1500">
+            <h1>H3 Headline</h1>
+                <p>
+                    Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet;
+                    illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, 
+                    ut ne quando amare inciperemus eum, 
+                    quem aliquando odisse possemus. Quin etiam si minus felices in diligendo fuissemus, 
+                    ferendum id Scipio potius quam inimicitiarum tempus cogitandum putabat.
+                </p>
+        </div>
+        <img src="view/img/bio3.JPG" alt="photo du coach" data-aos="fade-left" data-aos-duration="1500">
+    </div>
+    <script>AOS.init();</script>    
+</body>
+</html>
